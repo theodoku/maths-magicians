f631d2d4ce5cb7cdb2d8d24e8103f332cc5eedf8
theophilus doku <theodoku89@gmail.com>
2023-03-22 10:09:07 +0000

    deleted logo192.png, logo512.png, robots.txt and modified Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import {
   CALCULATOROPERATORS,
   NUM_1,
@@ -9,42 +8,35 @@ import {
 import Button from '../button/Button';
 import './Calculator.css';
 
-export default class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="cal-container">
-        <div className="cal-result">0</div>
-        <div>
-          {CALCULATOROPERATORS.map((item) => (
-            <Button key={item.id} title={item.operator} className={`btn-${item.id}`} />
-          ))}
-        </div>
-        <div>
-          {NUM_1.map((item) => (
-            <Button key={item.id} title={item.num} className={`btn-${item.id}`} />
-          ))}
-        </div>
-        <div>
-          {NUM_2.map((item) => (
-            <Button key={item.id} title={item.num} className={`btn-${item.id}`} />
-          ))}
-        </div>
-        <div>
-          {NUM_3.map((item) => (
-            <Button key={item.id} title={item.num} className={`btn-${item.id}`} />
-          ))}
-        </div>
-        <div>
-          {NUM_4.map((item) => (
-            <Button key={item.id} title={item.num} className={`btn-${item.id}`} />
-          ))}
-        </div>
+export default function Calculator() {
+  return (
+    <div className="cal-container">
+      <div className="cal-result">0</div>
+      <div>
+        {CALCULATOROPERATORS.map((item) => (
+          <Button key={item.id} title={item.operator} className={`btn-${item.id}`} />
+        ))}
       </div>
-    );
-  }
+      <div>
+        {NUM_1.map((item) => (
+          <Button key={item.id} title={item.num} className={`btn-${item.id}`} />
+        ))}
+      </div>
+      <div>
+        {NUM_2.map((item) => (
+          <Button key={item.id} title={item.num} className={`btn-${item.id}`} />
+        ))}
+      </div>
+      <div>
+        {NUM_3.map((item) => (
+          <Button key={item.id} title={item.num} className={`btn-${item.id}`} />
+        ))}
+      </div>
+      <div>
+        {NUM_4.map((item) => (
+          <Button key={item.id} title={item.num} className={`btn-${item.id}`} />
+        ))}
+      </div>
+    </div>
+  );
 }
