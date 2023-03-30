8559d47c33f0be723d587b3ef4b7e55fe011f3f5
theophilus doku <theodoku89@gmail.com>
2023-03-30 10:55:30 +0000

    modified operate.test.js, added a file for Calculator, Home, Navigation,Quotes.test.js

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -18,7 +18,7 @@ describe('operate', () => {
   });
 
   it('should throw error when dividing by 0', () => {
-    expect(operate(6, 0, '÷')).toThrow("Can't divide by 0.");
+    expect(operate(6, 0, '÷')).toBe("Can't divide by 0.");
   });
 
   it('should find modulo when dividing two numbers', () => {
@@ -26,10 +26,26 @@ describe('operate', () => {
   });
 
   it('should throw error when finding modulo and dividing by 0', () => {
-    expect(() => operate(7, 0, '%')).toThrow("Can't find modulo as can't divide by 0.");
+    expect(operate(7, 0, '%')).toBe("Can't find modulo as can't divide by 0.");
   });
 
   it('should throw error for unknown operation', () => {
     expect(() => operate(2, 3, '*')).toThrow("Unknown operation '*'");
+  });
+
+  it('should handle decimal addition', () => {
+    expect(operate(0.1, 0.2, '+')).toBe('0.3');
+  });
+
+  it('should handle decimal subtraction', () => {
+    expect(operate(0.3, 0.1, '-')).toBe('0.2');
+  });
+
+  it('should handle decimal multiplication', () => {
+    expect(operate(0.1, 0.2, 'x')).toBe('0.02');
+  });
+
+  it('should handle decimal division', () => {
+    expect(operate(0.1, 0.2, '÷')).toBe('0.5');
   });
 });
